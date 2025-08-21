4cb191550aeef65bec087f8adf84a662e3d8287c
João Magno <162519556+joaomagno1@users.noreply.github.com>
2025-08-20 22:20:45 -0300

    fimaula

---
+++ Exemplo IX/Form1.cs
@@ -0,0 +1,19 @@
+namespace Exemplo_IX
+{
+    public partial class Form1 : Form
+    {
+        float num1, num2, num3, num4, media;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        float calcMedia
+        {
+            media = (num1 + num2 + num3 + num4) / 4;
+        }
+        private void btnMedia_Click(object sender, EventArgs e)
+        {
+        txt
+        }
+    }
+}
