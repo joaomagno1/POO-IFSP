e1eafe7f4875293b9056b38cdb8ed055e705be65
João Magno <162519556+joaomagno1@users.noreply.github.com>
2025-10-15 20:25:26 -0300

    Add files via upload

---
+++ Introducao/Exemplo IX/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace Exemplo_IX
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNum4 = new Label();
+            lblNum3 = new Label();
+            lblNum2 = new Label();
+            lblNum1 = new Label();
+            txtNum1 = new TextBox();
+            txtNum2 = new TextBox();
+            txtNum3 = new TextBox();
+            txtNum4 = new TextBox();
+            btnMedia = new Button();
+            txtResultado = new TextBox();
+            SuspendLayout();
+            // 
+            // lblNum4
+            // 
+            lblNum4.AutoSize = true;
+            lblNum4.Location = new Point(50, 174);
+            lblNum4.Name = "lblNum4";
+            lblNum4.Size = new Size(63, 15);
+            lblNum4.TabIndex = 3;
+            lblNum4.Text = "Número 4:";
+            // 
+            // lblNum3
+            // 
+            lblNum3.AutoSize = true;
+            lblNum3.Location = new Point(50, 129);
+            lblNum3.Name = "lblNum3";
+            lblNum3.Size = new Size(63, 15);
+            lblNum3.TabIndex = 2;
+            lblNum3.Text = "Número 3:";
+            // 
+            // lblNum2
+            // 
+            lblNum2.AutoSize = true;
+            lblNum2.Location = new Point(50, 84);
+            lblNum2.Name = "lblNum2";
+            lblNum2.Size = new Size(63, 15);
+            lblNum2.TabIndex = 1;
+            lblNum2.Text = "Número 2:";
+            // 
+            // lblNum1
+            // 
+            lblNum1.AutoSize = true;
+            lblNum1.Location = new Point(50, 36);
+            lblNum1.Name = "lblNum1";
+            lblNum1.Size = new Size(63, 15);
+            lblNum1.TabIndex = 0;
+            lblNum1.Text = "Número 1:";
+            // 
+            // txtNum1
+            // 
+            txtNum1.Location = new Point(119, 31);
+            txtNum1.Name = "txtNum1";
+            txtNum1.Size = new Size(142, 23);
+            txtNum1.TabIndex = 4;
+            // 
+            // txtNum2
+            // 
+            txtNum2.Location = new Point(119, 76);
+            txtNum2.Name = "txtNum2";
+            txtNum2.Size = new Size(142, 23);
+            txtNum2.TabIndex = 5;
+            // 
+            // txtNum3
+            // 
+            txtNum3.Location = new Point(119, 124);
+            txtNum3.Name = "txtNum3";
+            txtNum3.Size = new Size(142, 23);
+            txtNum3.TabIndex = 6;
+            // 
+            // txtNum4
+            // 
+            txtNum4.Location = new Point(119, 169);
+            txtNum4.Name = "txtNum4";
+            txtNum4.Size = new Size(142, 23);
+            txtNum4.TabIndex = 7;
+            // 
+            // btnMedia
+            // 
+            btnMedia.Location = new Point(12, 222);
+            btnMedia.Name = "btnMedia";
+            btnMedia.Size = new Size(101, 23);
+            btnMedia.TabIndex = 8;
+            btnMedia.Text = "Média";
+            btnMedia.UseVisualStyleBackColor = true;
+            btnMedia.Click += btnMedia_Click;
+            // 
+            // txtResultado
+            // 
+            txtResultado.Enabled = false;
+            txtResultado.Location = new Point(119, 222);
+            txtResultado.Name = "txtResultado";
+            txtResultado.Size = new Size(142, 23);
+            txtResultado.TabIndex = 9;
+            txtResultado.TextChanged += txtResultado_TextChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(284, 271);
+            Controls.Add(txtResultado);
+            Controls.Add(btnMedia);
+            Controls.Add(txtNum4);
+            Controls.Add(txtNum3);
+            Controls.Add(txtNum2);
+            Controls.Add(txtNum1);
+            Controls.Add(lblNum4);
+            Controls.Add(lblNum3);
+            Controls.Add(lblNum2);
+            Controls.Add(lblNum1);
+            Name = "Form1";
+            Text = "Exercicío 4";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label lblNum4;
+        private Label lblNum3;
+        private Label lblNum2;
+        private Label lblNum1;
+        private TextBox txtNum1;
+        private TextBox txtNum2;
+        private TextBox txtNum3;
+        private TextBox txtNum4;
+        private Button btnMedia;
+        private TextBox txtResultado;
+    }
+}
